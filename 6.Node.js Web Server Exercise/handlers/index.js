116f688537566baa428e59db1162d5fac36146e1
georgiangelov2000 <angelov2000@abv.bg>
2021-02-18 00:25:15 +0200

    DELETED

--- 6.Node.js Web Server Exercise/handlers/index.js
+++
@@ -1,8 +0,0 @@
-let homeHandler=require('./home');
-let staticHandler=require('./static-files');
-let imageHandler=require('./images');
-module.exports=[
-    homeHandler,
-    staticHandler,
-    imageHandler
-]
