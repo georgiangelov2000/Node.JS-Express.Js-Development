8f7514f0a9bb4cae615af51a8f2a7bc6d3bbc961
georgiangelov2000 <angelov2000@abv.bg>
2021-02-19 11:14:17 +0200

    Updated Middleware

--- 7.Express.js Middleware/server.js
+++ 7.Express.js Middleware/server.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const app = express();
+const port = 3002;
 
-const myLogger = function (req, res, next) {
-    console.log('Logged');
-    next();
+/*
+app.use('/user/:id',function(req,res,next){
+    console.log('Request Url:',req.originalUrl)
+    next()
+},function(req,res,next){
+    console.log('Request Type:',req.method)
+    next()
+},function(req,res,next){
+    console.log('Id:',req.params.id)
+    next()
+});
+*/
+/*
+app.get('/user/:id',function(req,res,next){
+    res.send(`User Info: Method:${req.method}, Id:${req.params.id} `,)   
+})
+*/
+
+const requestTime = function (req, res, next) {
+    req.myTime = Date.now();
+    next()
 }
 
-const requestTime=function(req,res,next){
-    req.reqTime=Date.now()
-    next();
+const logOrinalUrl=function(req,res,next){
+    console.log('Request Url:',req.originalUrl)
+    next()
 }
+const logMethod=function(req,res,next){
+    console.log('Request Type:',req.method)
+    next()
+}
+let logStaff=[logOrinalUrl,logMethod]
 
-app.use(myLogger);
 app.use(requestTime);
 
-app.get('/',function(req,res){
-    res.send(`Current Time! ${req.reqTime}`)
+
+app.get('/', function (req, res) {
+    res.send(`Current Time! ${req.myTime}`);
+})
+app.get('/user/:id',logStaff,function(req,res,next){
+    res.send(`User Info: Method->${req.method} : Id->${req.params.id}`)
 })
 
-app.listen(3004,()=>console.log('app started on port 3004'));
+
+
+
+app.listen(port, () => console.log('app start on port 3002'))
