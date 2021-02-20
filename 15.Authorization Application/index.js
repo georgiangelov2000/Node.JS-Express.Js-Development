d7feebe61e3e3a5f6392f36dea0a56556b7af7f7
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2021-02-20 16:24:15 +0200

    added authorization app

---
+++ 15.Authorization Application/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+
+const router = require('./router');
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/route', router);
+
+// home route
+app.get('/', (req, res) =>{
+    res.render('index', { title : "Login System"});
+})
+
+app.listen(port, ()=>{ console.log("Lostening to the server on http://localhost:3000")});
