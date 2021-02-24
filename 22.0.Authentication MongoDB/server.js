aa91d910756e02dfb1b8cc1d584458e710a5081a
George <angelov2000@abv.bg>
2021-02-24 12:37:07 +0200

    Updated

--- 22.0.Authentication MongoDB/server.js
+++ 22.0.Authentication MongoDB/server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./server/config/database");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "dsadnsadnjksandjknsadjnsajkd";
 
 mongoose.connect(
   "mongodb+srv://angelov2000:angelov2001@newcluster.cxigb.mongodb.net/authentication?retryWrites=true&w=majority",
@@ -18,26 +20,52 @@ mongoose.connect(
 app.use("/", express.static(path.join(__dirname, "views")));
 app.use(bodyParser.json());
 
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username}).lean();
+  //console.log(user)
+  //console.log(username,password)
+
+  if (!user) {
+    return res.json({ status: "error", error: "Invalud username or password" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      JWT_SECRET
+    );
+    return res.json({ status: 'ok',data: token });
+  }
+ // console.log(token)
+  res.json({status:'error',error:'Invalid username||password'})
+});
+
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
   const { username, password: myPassword } = req.body;
 
-  if(!username||typeof username!=='string'){
-    return res.json({status:'error',error:'Invalid username'})
+  if (!username || typeof username !== "string") {
+    return res.json({ status: "error", error: "Invalid username" });
   }
   if (myPassword.length < 5) {
-    return res.json({status: "error",error: "Password too small.Please try again latter!",});
+    return res.json({
+      status: "error",
+      error: "Password too small.Please try again latter!",
+    });
   }
 
-  const password=await bcrypt.hash(myPassword,10)
-  console.log(password)
-  
+  const password = await bcrypt.hash(myPassword, 10);
+  console.log(password);
+
   try {
     const response = await User.create({ username, password });
     console.log(response);
   } catch (error) {
     if (error.code === 11000) {
-    return res.json({ status: "error", error: "Username already in use" });
+      return res.json({ status: "error", error: "Username already in use" });
     }
   }
   res.json({ status: "ok" });
